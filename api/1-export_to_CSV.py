be5d477221a7efbb0f6a024691d3ab859a82ba99
eumunyana <e.umunyana@alustudent.com>
2023-07-07 19:30:58 +0000

    1. Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-""" Call API and store data in CSV """
+"""
+Uses https://jsonplaceholder.typicode.com along with an employee ID to
+return information about the employee's todo list progress
+"""
+
 import csv
 import requests
 from sys import argv
 
-
 if __name__ == '__main__':
-    employee_id = argv[1]
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-    todo = requests.get(url_todo, params={'userId': employee_id})
-    user = requests.get(url_user, params={'id': employee_id})
-
-    todo_dict_list = todo.json()
-    user_dict_list = user.json()
-
-    employee = user_dict_list[0].get('username')
-
-    with open("{}.csv".format(employee_id), "a+") as csvfile:
-        csvwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for task in todo_dict_list:
-            status = task['completed']
-            title = task['title']
-            csvwriter.writerow(["{}".format(employee_id),
-                                "{}".format(employee),
-                                "{}".format(status),
-                                "{}".format(title)])
+    userId = argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".
+                        format(userId), verify=False).json()
+    todo = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}".
+                        format(userId), verify=False).json()
+    with open("{}.csv".format(userId), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todo:
+            taskwriter.writerow([int(userId), user.get('username'),
+                                 task.get('completed'),
+                                 task.get('title')])
