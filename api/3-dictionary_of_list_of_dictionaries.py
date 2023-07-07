9c526140b8bc133611d70e52cd7d2be4bcc2fd14
eumunyana <e.umunyana@alustudent.com>
2023-07-07 19:38:40 +0000

    3. Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,75 +1,27 @@
 #!/usr/bin/python3
+"""Exports data in the JSON format"""
 
-""" Calls an API and stores data in a JSON file """
+if __name__ == "__main__":
 
+    import json
+    import requests
+    import sys
 
-import csv
-import json
-import requests
+    users = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users.json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
+    todoAll = {}
 
+    for user in users:
+        taskList = []
+        for task in todos:
+            if task.get('userId') == user.get('id'):
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
 
-if __name__ == '__main__':
-    userId = 1
-    user_tasks = {}
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-    users = requests.get(url_user).json()
-
-    for userId in range(1, len(users) + 1):
-        todo = requests.get(url_todo, params={'userId': userId})
-        user = requests.get(url_user, params={'id': userId})
-
-        todo_dict_list = todo.json()
-        user_dict_list = user.json()
-        task_list = []
-        employee = user_dict_list[0].get('username')
-
-        for task in todo_dict_list:
-            status = task.get('completed')
-            title = task.get('title')
-            task_dict = {}
-            task_dict['task'] = title
-            task_dict['completed'] = status
-            task_dict['username'] = employee
-            task_list.append(task_dict)
-        user_tasks[userId] = task_list
-
-    with open("todo_all_employees.json", "w+") as jsonfile:
-        json.dump(user_tasks, jsonfile)#!/usr/bin/python3
-
-
-# Calls an API and stores data in a JSON file 
-
-import csv
-import json
-import requests
-
-
-if __name__ == '__main__':
-    userId = 1
-    user_tasks = {}
-    url_todo = 'https://jsonplaceholder.typicode.com/todos/'
-    url_user = 'https://jsonplaceholder.typicode.com/users/'
-    users = requests.get(url_user).json()
-
-    for userId in range(1, len(users) + 1):
-        todo = requests.get(url_todo, params={'userId': userId})
-        user = requests.get(url_user, params={'id': userId})
-
-        todo_dict_list = todo.json()
-        user_dict_list = user.json()
-        task_list = []
-        employee = user_dict_list[0].get('username')
-
-        for task in todo_dict_list:
-            status = task.get('completed')
-            title = task.get('title')
-            task_dict = {}
-            task_dict['task'] = title
-            task_dict['completed'] = status
-            task_dict['username'] = employee
-            task_list.append(task_dict)
-        user_tasks[userId] = task_list
-
-    with open("todo_all_employees.json", "w+") as jsonfile:
-        json.dump(user_tasks, jsonfile)
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
